16a406bda6f639d0ad89d37e59ad59e1155c2ef5
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-06-22 15:42:52 +0300

    initial products page

---
+++ src/pages/products/list/index.js
@@ -0,0 +1,80 @@
+import SortableTable from '../../../components/sortable-table/index.js';
+import header from './products-header.js';
+
+import fetchJson from '../../../utils/fetch-json.js';
+
+export default class Page {
+  element;
+  subElements = {};
+  components = {};
+
+  async updateTableComponent (from, to) {
+    const data = await fetchJson(`${process.env.BACKEND_URL}api/dashboard/bestsellers?_start=1&_end=20&from=${from.toISOString()}&to=${to.toISOString()}`);
+    this.components.sortableTable.addRows(data);
+  }
+
+  async initComponents () {
+    const to = new Date();
+    const from = new Date(to.getTime() - (30 * 24 * 60 * 60 * 1000));
+
+    const sortableTable = new SortableTable(header, {
+      url: `api/dashboard/bestsellers?_start=1&_end=20&from=${from.toISOString()}&to=${to.toISOString()}`,
+      isSortLocally: true
+    });
+
+    this.components.sortableTable = sortableTable;
+  }
+
+  get template () {
+    return `
+    <div class="products-list">
+      <div class="content__top-panel">
+        <h1 class="page-title">Товары</h1>
+        <a href="/products/add" class="button-primary">Добавить товар</a>
+      </div>
+      <div data-element="sortableTable">
+        <!-- sortable-table component -->
+      </div>
+    </div>`;
+  }
+
+  async render () {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
+
+    await this.initComponents();
+
+    this.renderComponents();
+
+    return this.element;
+  }
+
+  renderComponents () {
+    Object.keys(this.components).forEach(component => {
+      const root = this.subElements[component];
+      const { element } = this.components[component];
+
+      root.append(element);
+    });
+  }
+
+  getSubElements ($element) {
+    const elements = $element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  destroy () {
+    for (const component of Object.values(this.components)) {
+      component.destroy();
+    }
+  }
+}
