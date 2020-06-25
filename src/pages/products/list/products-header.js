55545429045a0a5180cd0d5a763d842dda93618d
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-06-25 17:09:20 +0300

    added subcategory colunm in the table of products page. work in progress

--- src/pages/products/list/products-header.js
+++ src/pages/products/list/products-header.js
@@ -18,6 +18,26 @@ const header = [
     sortType: 'string'
   },
   {
+    id: 'subcategory',
+    title: 'Subcategory',
+    sortable: false,
+    template: data => {
+      return `
+        <div class="sortable-table__cell">
+          <span data-tooltip=
+            "
+              <div class=&quot;sortable-table-tooltip&quot;>
+                <span class=&quot;sortable-table-tooltip__category&quot;>Компьютеры</span> /
+                <b class=&quot;sortable-table-tooltip__subcategory&quot;>Компьютеры, ноутбуки и ПО</b>
+              </div>
+            ">
+            ${data.title}
+          </span>
+        </div>
+        `;
+    }
+  },
+  {
     id: 'quantity',
     title: 'Quantity',
     sortable: true,
