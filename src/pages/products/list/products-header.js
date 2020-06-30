a5ce3ce8d2b0956171486c8b5f3e11385ec4f985
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-06-30 18:38:55 +0300

    added additional check whether or not subcategory and category are undefined for products

--- src/pages/products/list/products-header.js
+++ src/pages/products/list/products-header.js
@@ -24,15 +24,17 @@ const header = [
     template: data => {
       return `
         <div class="sortable-table__cell">
-          <span data-tooltip=
-            "
-              <div class=&quot;sortable-table-tooltip&quot;>
-                <span class=&quot;sortable-table-tooltip__category&quot;>${data.category.title}</span> /
-                <b class=&quot;sortable-table-tooltip__subcategory&quot;>${data.title}</b>
-              </div>
-            ">
-            ${data.title}
-          </span>
+          ${(data.category && data.title)  
+            ? `<span data-tooltip=
+              "
+                <div class=&quot;sortable-table-tooltip&quot;>
+                  <span class=&quot;sortable-table-tooltip__category&quot;>${(data.category) ? data.category.title : ''}</span> /
+                  <b class=&quot;sortable-table-tooltip__subcategory&quot;>${data.title}</b>
+                </div>
+              ">
+                ${data.title}
+              </span> `
+            : "-"}
         </div>
         `;
     }
