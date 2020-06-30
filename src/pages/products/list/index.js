c0c62692c5fd36dd51a92454846135f132d3563a
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-06-30 18:40:04 +0300

    sortPanel based filtering started working. WIP

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,19 +1,98 @@
 import SortableTable from '../../../components/sortable-table/index.js';
 import SortPanel from '../../../components/sort-panel/index.js';
 import header from './products-header.js';
+import fetchJson from '../../../utils/fetch-json.js';
 
-//import fetchJson from '../../../utils/fetch-json.js';
+const BACKEND_URL = process.env.BACKEND_URL;
+const PRODUCTS_URL = "api/rest/products";
 
 export default class Page {
   element;
   subElements = {};
   components = {};
+  sliderFrom = 0;
+  sliderTo = 4000;
+
+  updateTableComponent = async (event) => {
+    const { type, detail } = event;
+
+    if (type === "range-select") {
+      const { from, to } = detail;
+      this.sliderFrom = from;
+      this.sliderTo = to;
+    }    
+    
+    const { sorted, start, end, element: sortableTableElem } = this.components.sortableTable;
+    const { id: sort, order } = sorted;
+    
+    sortableTableElem.classList.add("sortable-table_loading");
+    
+    const { subElements } = this.components.sortPanel;
+    const { filterName, filterStatus } = subElements;
+    const { value: filterNameValue } = filterName;
+    const { value: filterStatusValue } = filterStatus;
+
+ 
+
+    const url = new URL(PRODUCTS_URL, BACKEND_URL);
+
+    const searchParams = [
+      ["price_gte", this.sliderFrom],
+      ["price_lte", this.sliderTo]
+    ];
+
+    if (filterNameValue) {
+      searchParams.push(
+        ["title_like", filterNameValue]
+      )
+    }
+
+    if (filterStatusValue) {
+      searchParams.push(
+        ["status", filterStatusValue]
+      )
+    }
+
+    //TODO: сделать через цикл
+
+    searchParams.push([
+      "_sort",
+      sort
+    ]);
+
+    searchParams.push([
+      "_order",
+      order
+    ]);
+
+    searchParams.push([
+      "_start",
+      start
+    ]);
+
+    searchParams.push([
+      "_end",
+      end
+    ]);
+
+    for (const [name, value] of searchParams) {
+      url.searchParams.set(name, value);
+    }
+
+    const data = await fetchJson(url);
+    this.components.sortableTable.addRows(data);
+
+    sortableTableElem.classList.remove("sortable-table_loading");
+  }
 
   async initComponents () {
     const to = new Date();
     const from = new Date(to.getTime() - (30 * 24 * 60 * 60 * 1000));
 
-    const sortPanel = new SortPanel();
+    const sortPanel = new SortPanel({
+      sliderMin: this.sliderFrom,
+      sliderMax: this.sliderTo
+    });
 
     const sortableTable = new SortableTable(header, {
       url: `api/rest/products?_embed=subcategory.category&_sort=title&_order=asc&_start=0&_end=30`
@@ -51,7 +130,31 @@ export default class Page {
 
     this.renderComponents();
 
+    this.initEventListeners();
+
     return this.element;
+  }
+
+  initEventListeners() {
+    const { subElements } = this.components.sortPanel;
+    const { filterName, filterStatus } = subElements;
+
+    for (const element of [filterName, filterStatus]) {
+      element.addEventListener("input", this.updateTableComponent);
+    }
+
+    this.element.addEventListener("range-select", this.updateTableComponent);
+  }
+
+  removeEventListeners() {
+    const { subElements } = this.components.sortPanel;
+    const { filterName, filterStatus } = subElements;
+
+    for (const element of [filterName, filterStatus]) {
+      element.removeEventListener("input", this.updateTableComponent);
+    }
+
+    this.element.removeEventListener("range-select", this.updateTableComponent);
   }
 
   renderComponents () {
@@ -77,5 +180,6 @@ export default class Page {
     for (const component of Object.values(this.components)) {
       component.destroy();
     }
+    this.removeEventListeners();
   }
 }
