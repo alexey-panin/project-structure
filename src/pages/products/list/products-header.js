035658ed1e39caec3bb92e7c8abd60625b2a27a6
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-06-30 19:21:00 +0300

    added template for a price to show it in dollars

--- src/pages/products/list/products-header.js
+++ src/pages/products/list/products-header.js
@@ -49,7 +49,10 @@ const header = [
     id: 'price',
     title: 'Price',
     sortable: true,
-    sortType: 'number'
+    sortType: 'number',
+    template: data => {
+      return `<div class="sortable-table__cell">$${data}</div>`;
+    }
   },
   {
     id: 'status',
